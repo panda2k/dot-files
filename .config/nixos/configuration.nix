b57ef6960482a70103f34544f5f02f1f1c3d4e85
Michael Wang <thelowkeypanda@gmail.com>
2024-11-20 23:43:16 -0800

    get airpods bluetooth

--- .config/nixos/configuration.nix
+++ .config/nixos/configuration.nix
@@ -83,6 +83,9 @@ in
     alsa.support32Bit = true;
     jack.enable = true;
   };
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
 
   # Enable touchpad support (enabled default in most desktopManager).
   # services.libinput.enable = true;
